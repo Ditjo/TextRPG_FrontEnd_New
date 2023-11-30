44ff73adc4a48b0cd77086eca21685876cdeaee5
Ditjo <mads.ditjo@gmail.com>
2023-11-30 14:14:22 +0100

    -added gethero in admin panel

--- src/app/components/admin-panel/admin-panel.component.ts
+++ src/app/components/admin-panel/admin-panel.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ArmourService } from '../../services/armour.service';
 import { Armour } from '../../models/armour';
+import { HeroService } from '../../services/hero.service';
+import { Hero } from '../../models/hero';
 
 @Component({
   selector: 'app-admin-panel',
@@ -9,18 +11,30 @@ import { Armour } from '../../models/armour';
 })
 export class AdminPanelComponent {
 
-constructor(private armourService:ArmourService){
+constructor(
+  private armourService:ArmourService,
+  private heroService:HeroService
+  ){
 
 }
 
 armourlist:Armour[] = [];
-
+herolist:Hero[] = []
 
 GetAllArmour(){
   this.armourService.GetAll().subscribe(
     (data) =>{
       this.armourlist = data;
       console.log(this.armourlist);
+    }
+  )
+}
+
+GetAllHeros(){
+  this.heroService.GetAll().subscribe(
+    (data) =>{
+      this.herolist = data;
+      console.log(this.herolist);
     }
   )
 }
