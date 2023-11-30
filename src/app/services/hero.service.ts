83f3552fbb076cf884c814684fdf3e0e3d4e91de
Ditjo <mads.ditjo@gmail.com>
2023-11-30 14:02:51 +0100

    -added heroService

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environment/environment';
+import { Hero } from '../models/hero';
+import { Observable } from 'rxjs';
+import { httpOptions } from '../environment/httpOptions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  private readonly apiUrl=environment.apiUrl+"Hero/";
+  constructor(private http:HttpClient) { }
+
+  GetAll():Observable<Hero[]>{
+    return this.http.get<Hero[]>(this.apiUrl);
+  }
+
+  GetById(id:number):Observable<Hero>{
+    return this.http.get<Hero>(this.apiUrl+id);
+  }
+
+  Delete(id:number):Observable<void>{
+    let i = this.http.delete<void>(this.apiUrl+id);
+    return i;
+  }
+
+  Create(armour:Hero):Observable<void>{
+    return this.http.post<void>(this.apiUrl, armour, httpOptions)
+  }
+
+  Update(armour:Hero):Observable<void>{
+    return this.http.put<void>(this.apiUrl+armour.id, armour, httpOptions)
+  }
 }
