31921d9daf4e7315a5f95cd55fa8a74c1e93a898
Ditjo <mads.ditjo@gmail.com>
2023-12-08 11:28:33 +0100

    main-game basic view to show the selected hero is in the game

--- src/app/components/main-game/main-game.component.ts
+++ src/app/components/main-game/main-game.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Hero } from '../../models/hero';
+import { GenericService } from '../../services/generic.service';
+import { TableURL } from '../../tools/table-url';
 
 @Component({
   selector: 'app-main-game',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class MainGameComponent {
 
+  constructor(
+    private heroService:GenericService<Hero>,
+  ){
+    
+  }
+  ngOnInit(){
+
+
+    let i = localStorage.getItem('HeroToGame')
+
+    if (i != null ){
+      this.heroService.getById(TableURL.Hero, i as unknown as number).subscribe(
+        (data)=> {
+          this.playingHero = data;
+        }
+      )
+    localStorage.removeItem('HeroToGame')
+  }
+  }
+
+  playingHero?:Hero
 }
